efdc8cd91e0d9307f86dc8c47e450c259d1993ff
Dinesh-Gautam <dineshgautam5252@gmail.com>
2023-08-08 20:26:57 +0530

    added mouse scale animation

--- src/components/FollowMouse.tsx
+++ src/components/FollowMouse.tsx
@@ -3,14 +3,14 @@ import React, { useEffect, useRef, useState } from 'react'
 
 function FollowMouse() {
     const divRef   = useRef<HTMLDivElement | null>(null);
-    const mousePosition = { x : 0 ,  y : 0,}
+    const mousePosition = { x : 0 ,  y : 0, scale : 0,}
     const intervaleRef = useRef<number | null>(null);
 
     const  updateElementPosition: TimerHandler = () =>  {
         const div = divRef.current;
         if(div === null) return;
         div.style.opacity = "1";
-        div.style.transform = `translate(${mousePosition.x}px, ${mousePosition.y}px)`;
+        div.style.transform = `translate(${mousePosition.x}px, ${mousePosition.y}px) scale(${mousePosition.scale})`;
       }
 
 
@@ -21,13 +21,21 @@ function FollowMouse() {
   
         // Calculate the position of the cursor relative to the div
         if(div !== null) {
-  
-          const rect = div.getBoundingClientRect();
-          const mouseX = clientX -(rect.width / 2); 
-          const mouseY = clientY - (rect.height / 2)
+          const mouseX = clientX -(div.offsetHeight / 2); 
+          const mouseY = clientY - (div.offsetHeight / 2)
     
           mousePosition.x = mouseX;
           mousePosition.y = mouseY;
+          const path  = event.composedPath() ;
+          if( divRef.current  ) {
+            if(path.some((target) => target !== null && ((target as HTMLAnchorElement).tagName === "A" || (target as HTMLHeadingElement).tagName === "H1"))) {
+              mousePosition.scale = 2;
+            }else {
+              mousePosition.scale = 1;
+            }
+          
+          }
+          
           
           if(intervaleRef.current === null ) intervaleRef.current = window.setInterval(updateElementPosition , 50);
         }
